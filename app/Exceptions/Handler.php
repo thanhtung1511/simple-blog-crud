d9ba9eed8c26a7ca1666f8b2f90dd0584d37e8e7
Tung Phi Thanh <thanhtung.1511@gmail.com>
2020-02-20 19:56:39 +0700

    Update exception handler and add general exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,16 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 
+/**
+ * Class Handler.
+ */
 class Handler extends ExceptionHandler
 {
     /**
@@ -13,7 +21,7 @@ class Handler extends ExceptionHandler
      * @var array
      */
     protected $dontReport = [
-        //
+        GeneralException::class,
     ];
 
     /**
@@ -29,10 +37,10 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
-     * @return void
+     * @param Exception $exception
      *
-     * @throws \Exception
+     * @throws Exception
+     * @return mixed|void
      */
     public function report(Exception $exception)
     {
@@ -50,6 +58,25 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof UnauthorizedException) {
+            return redirect()
+                ->route(home_route())
+                ->withFlashDanger(__('auth.general_error'));
+        }
+
         return parent::render($request, $exception);
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException  $exception
+     *
+     * @return JsonResponse|RedirectResponse
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $request->expectsJson()
+            ? response()->json(['message' => 'Unauthenticated.'], 401)
+            : redirect()->guest(route('frontend.auth.login'));
     }
 }
