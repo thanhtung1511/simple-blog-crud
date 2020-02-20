c7169b3c5fcd5c5b61b5698266031de09244a71c
Tung Phi Thanh <thanhtung.1511@gmail.com>
2020-02-20 19:26:03 +0700

    Add helpers and view composers

---
+++ app/Helpers/GlobalHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+if (! function_exists('app_name')) {
+    /**
+     * Helper to grab the application name.
+     *
+     * @return mixed
+     */
+    function app_name()
+    {
+        return config('app.name');
+    }
+}
+
+if (! function_exists('home_route')) {
+    /**
+     * Return the route to the "home" page depending on authentication/authorization status.
+     *
+     * @return string
+     */
+    function home_route()
+    {
+        if (auth()->check()) {
+            if (auth()->user()->isAdmin()) {
+                return 'admin.dashboard';
+            }
+
+            return 'frontend.user.dashboard';
+        }
+
+        return 'frontend.index';
+    }
+}
