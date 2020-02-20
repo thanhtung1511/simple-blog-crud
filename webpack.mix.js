3215bbc33f1e60eea11bd7c878c06521a8c3f1cc
Tung Phi Thanh <thanhtung.1511@gmail.com>
2020-02-20 22:36:53 +0700

    Update mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,43 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.setPublicPath('public')
+    .setResourceRoot('../') // Turns assets paths in css relative to css file
+    // .options({
+    //     processCssUrls: false,
+    // })
+    .sass('resources/sass/frontend/app.scss', 'css/frontend.css')
+    .sass('resources/sass/backend/app.scss', 'css/backend.css')
+    .js('resources/js/frontend/app.js', 'js/frontend.js')
+    .js([
+        'resources/js/backend/before.js',
+        'resources/js/backend/app.js',
+        'resources/js/backend/after.js'
+    ], 'js/backend.js')
+    .extract([
+        // Extract packages from node_modules to vendor.js
+        'jquery',
+        'bootstrap',
+        'popper.js',
+        'axios',
+        'sweetalert2',
+        'lodash'
+    ])
+    .sourceMaps();
+
+if (mix.inProduction()) {
+    mix.version()
+        .options({
+            // Optimize JS minification process
+            terser: {
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }
+        });
+} else {
+    // Uses inline source-maps on development
+    mix.webpackConfig({
+        devtool: 'inline-source-map'
+    });
+}
