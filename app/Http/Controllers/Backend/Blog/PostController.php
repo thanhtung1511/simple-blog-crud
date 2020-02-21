01a00c4def09d738aff601a80cc4a481c4e138f6
Tung Phi Thanh <thanhtung.1511@gmail.com>
2020-02-21 19:34:01 +0700

    Refactor admin post controller

--- app/Http/Controllers/Backend/Blog/PostController.php
+++ app/Http/Controllers/Backend/Blog/PostController.php
@@ -4,23 +4,10 @@ namespace App\Http\Controllers\Backend\Blog;
 
 use App\Exceptions\GeneralException;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Backend\Role\ManageRoleRequest;
-use App\Http\Requests\Backend\Role\StoreRoleRequest;
-use App\Http\Requests\Backend\Role\UpdateRoleRequest;
-use App\Http\Requests\Frontend\Post\ManagePostRequest;
-use App\Http\Requests\Frontend\Post\UpdatePostRequest;
 use App\Models\Post;
-use App\Models\Role;
 use App\Repositories\Contracts\PostRepositoryContract;
-use Carbon\Carbon;
-use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
-use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
-use Storage;
-use Throwable;
 
 class PostController extends Controller
 {
